b08502be726e318b1f3b710b74781eee79d6e25b
Voldisoriginal <1voldis1@gmail.com>
2024-09-05 14:29:00 +0500

    Adding protection against mail abuse

--- ServerApp/Data/ApplicationUser.cs
+++ ServerApp/Data/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace ServerApp.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public DateTimeOffset LastEmailSentDate { get; set; }
     }
 
 }
